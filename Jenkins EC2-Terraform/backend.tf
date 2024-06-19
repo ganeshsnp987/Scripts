3cc35747838df44a066ebb2f18b592782179a787
Ganesh Sanap <114701526+ganeshsnp987@users.noreply.github.com>
2024-06-19 09:43:46 +0530

    Update backend.tf

--- Jenkins EC2-Terraform/backend.tf
+++ Jenkins EC2-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "backend_s3_storage" # Replace with your actual S3 bucket name
+    bucket = "tetris-backend" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
