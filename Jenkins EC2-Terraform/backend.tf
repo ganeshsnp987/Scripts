1f3c6cd2153e8a01e9b2fb9a28609a15c1dcf4b6
Ganesh Sanap <114701526+ganeshsnp987@users.noreply.github.com>
2024-06-22 09:31:24 +0530

    Update backend.tf

--- Jenkins EC2-Terraform/backend.tf
+++ Jenkins EC2-Terraform/backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket = "tetris-backend" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
-    dynamodb_table = "mydb_table"
   }
 }
